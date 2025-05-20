1224f23815d6570836062ae5ae6bf5425adaa211
Pathik Sutaria <pathik09@gmail.com>
2025-05-20 17:01:25 +0530

    formatting

--- src/main/java/com/staah/hotelmanagement/model/ActualBooking.java
+++ src/main/java/com/staah/hotelmanagement/model/ActualBooking.java
@@ -5,45 +5,52 @@ import java.util.List;
 
 public class ActualBooking {
 	private Hotel hotel;
-    private List<LocalDate> bookingDates;
+	private List<LocalDate> bookingDates;
 	private RoomType roomType;
 	private String RoomRate;
+
 	public Hotel getHotel() {
 		return hotel;
 	}
+
 	public void setHotel(Hotel hotel) {
 		this.hotel = hotel;
 	}
-	public ActualBooking(Hotel hotel,List<LocalDate> bookingDates, RoomType roomType,
-			String roomRate) {
+
+	public ActualBooking(Hotel hotel, List<LocalDate> bookingDates, RoomType roomType, String roomRate) {
 		super();
 		this.hotel = hotel;
 		this.bookingDates = bookingDates;
 		this.roomType = roomType;
 		RoomRate = roomRate;
 	}
-	
+
 	public ActualBooking() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public List<LocalDate> getBookingDates() {
 		return bookingDates;
 	}
+
 	public void setBookingDates(List<LocalDate> bookingDates) {
 		this.bookingDates = bookingDates;
 	}
+
 	public RoomType getRoomType() {
 		return roomType;
 	}
+
 	public void setRoomType(RoomType roomType) {
 		this.roomType = roomType;
 	}
+
 	public String getRoomRate() {
 		return RoomRate;
 	}
+
 	public void setRoomRate(String roomRate) {
 		RoomRate = roomRate;
 	}
-	
 
 }
--- src/main/java/com/staah/hotelmanagement/model/Room.java
+++ src/main/java/com/staah/hotelmanagement/model/Room.java
@@ -2,18 +2,19 @@ package com.staah.hotelmanagement.model;
 
 public class Room {
 	private String roomId;
-	
-	
 
 	public String getRoomType() {
 		return roomType;
 	}
+
 	public String getRoomId() {
 		return roomId;
 	}
+
 	public void setRoomId(String roomId) {
 		this.roomId = roomId;
 	}
+
 	public void setRoomType(String roomType) {
 		this.roomType = roomType;
 	}
