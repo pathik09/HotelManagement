2c39a87bd13d6b24f6a44c30d23ad19eb175708b
Pathik Sutaria <pathik09@gmail.com>
2025-05-20 16:48:12 +0530

    adding testcase

--- src/test/java/com/staah/hotelmanagement/createobject/CreateBookingObjectTest.java
+++ src/test/java/com/staah/hotelmanagement/createobject/CreateBookingObjectTest.java
@@ -1,25 +1,31 @@
 package com.staah.hotelmanagement.createobject;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import com.staah.hotelmanagement.model.Hotel;
 
+import com.staah.hotelmanagement.model.Hotel;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.staah.hotelmanagement.model.Booking;
 
 class CreateBookingObjectTest {
 
-	
-
 	@Test
 	void testGetActualBooking() {
+
 		CreateBookingObject createBookingObject = Mockito.mock(CreateBookingObject.class);
-        assertNotNull(createBookingObject.getActualBooking(new ArrayList<Hotel>()));
+		Gson gson = Mockito.mock(Gson.class);
+		FileReader fileReader = Mockito.mock(FileReader.class);
+		TypeToken type = Mockito.mock(TypeToken.class);
+		Mockito.when(gson.fromJson(fileReader, type)).thenReturn(new ArrayList<Booking>());
+		assertNotNull(createBookingObject.getActualBooking(new ArrayList<Hotel>()));
 	}
 
 }
